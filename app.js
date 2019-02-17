c61c59c712ea796bd07cf424fb08763aa6c24fd7
Jesse Ragos <jkiel.r4gos@gmail.com>
2019-02-17 17:59:37 +0800

    Update design

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var express             = require("express"),
-    app                 = express(),
-    bodyParser          = require("body-parser"),
-    mongoose            = require("mongoose"),
-    flash               = require("connect-flash"),
-    passport            = require("passport"),
-    LocalStrategy       = require("passport-local"),
-    methodOverride      = require("method-override"),
-    User                = require("./models/user"),
-    seedDB              = require("./seeds");
-    
-var commentRoutes       = require("./routes/comments"),
-    campgroundRoutes    = require("./routes/campgrounds"),
-    indexRoutes         = require("./routes/index");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
+
+var commentRoutes = require("./routes/comments"),
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
 
 /*
     =====================
@@ -21,10 +21,10 @@ var commentRoutes       = require("./routes/comments"),
 // Empty and seed DB
 // seedDB();
 
-// Connect/Create yelp_camp db 
+// Connect/Create yelp_camp db
 // mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 // mongoose.connect("mongodb://jkieluser1:jkielpword1@ds013014.mlab.com:13014/yelpcamp_jkiel9", { useNewUrlParser: true });
-require('dotenv').config();
+require("dotenv").config();
 var dbUrl = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
 mongoose.connect(dbUrl, { useNewUrlParser: true });
 console.log("DB in use: " + dbUrl);
@@ -33,7 +33,7 @@ console.log("DB in use: " + dbUrl);
     EXPRESS SETUP
     =====================
 */
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "/public"));
 
@@ -49,11 +49,13 @@ app.set("view engine", "ejs");
     EXPRESS SESSION CONFIG
     =====================
 */
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "This is a secret",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 
 /*
     =====================
@@ -67,11 +69,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // Add middleware function to all templates rendered
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;  //  Save user to local environment of template for access
-    res.locals.error = req.flash("error");    //  Make flash message available at locals
-    res.locals.success = req.flash("success");
-    next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user; //  Save user to local environment of template for access
+  res.locals.error = req.flash("error"); //  Make flash message available at locals
+  res.locals.success = req.flash("success");
+  next();
 });
 
 /*
@@ -87,11 +89,12 @@ app.use("/campgrounds/:id/comments", commentRoutes);
     =====================
         CALLBACK ROUTE
     =====================
-*/ 
+*/
+
 app.get("*", function(req, res) {
-   res.send("ThiS pAgE has Error: " + res.statusCode);
+  res.send("ThiS pAgE has Error: " + res.statusCode);
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("Server Started..."); 
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("Server started in port " + process.env.PORT);
 });
